64f2072a77c4be1b2e98d5a69e49d0de01cd092c
Chris Clements <cclements@users.noreply.github.com>
2026-08-11 16:10:28 -0500

    Parse PSSH boxes strictly

--- src/Mpeg4Lib/Boxes/PsshBox.cs
+++ src/Mpeg4Lib/Boxes/PsshBox.cs
@@ -6,27 +6,80 @@ namespace Mpeg4Lib.Boxes;
 
 public class PsshBox : FullBox
 {
-	public override long RenderSize => base.RenderSize + 16 + sizeof(int) + InitData.Length + ExtraData.Length;
+	public override long RenderSize => Header.HeaderSize + sizeof(uint) + 16
+		+ (Version == 1 ? sizeof(uint) + 16L * KIDs.Length : 0)
+		+ sizeof(uint) + InitData.Length;
 	public Guid ProtectionSystemId { get; }
+	public Guid[] KIDs { get; }
 	public byte[] InitData { get; }
-	public byte[] ExtraData { get; }
+	public byte[] ExtraData => Array.Empty<byte>();
 
 	public PsshBox(Stream file, BoxHeader header, IBox? parent) : base(file, header, parent)
 	{
-		ProtectionSystemId = new Guid(file.ReadBlock(16), bigEndian: true);
-		int initDataSize = file.ReadInt32BE();
-		InitData = file.ReadBlock(initDataSize);
+		long fullBoxHeaderSize = (long)header.HeaderSize + sizeof(uint);
+		if (header.TotalBoxSize < fullBoxHeaderSize)
+			throw new InvalidDataException(
+				$"pssh size {header.TotalBoxSize} is smaller than its {fullBoxHeaderSize}-byte header.");
 
-		var remaining = (int)(header.TotalBoxSize - header.HeaderSize - 4 - 16 - sizeof(int) - initDataSize);
-		ExtraData = file.ReadBlock(remaining);
+		long remaining = header.TotalBoxSize - fullBoxHeaderSize;
+		if (Version is not (0 or 1))
+			throw new InvalidDataException($"Unsupported pssh version {Version}.");
+
+		EnsureRemaining(remaining, 16, nameof(ProtectionSystemId));
+		ProtectionSystemId = new Guid(file.ReadBlock(16), bigEndian: true);
+		remaining -= 16;
+
+		if (Version == 1)
+		{
+			EnsureRemaining(remaining, sizeof(uint), "KID_count");
+			uint kidCount = file.ReadUInt32BE();
+			remaining -= sizeof(uint);
+
+			if (kidCount > int.MaxValue)
+				throw new InvalidDataException($"pssh KID_count {kidCount} exceeds the supported array size.");
+
+			long kidBytes = 16L * kidCount;
+			EnsureRemaining(remaining, kidBytes + sizeof(uint), "KIDs and DataSize");
+			KIDs = new Guid[(int)kidCount];
+			for (int i = 0; i < KIDs.Length; i++)
+				KIDs[i] = new Guid(file.ReadBlock(16), bigEndian: true);
+			remaining -= kidBytes;
+		}
+		else
+		{
+			KIDs = [];
+		}
+
+		EnsureRemaining(remaining, sizeof(uint), "DataSize");
+		uint initDataSize = file.ReadUInt32BE();
+		remaining -= sizeof(uint);
+		if (remaining != initDataSize)
+			throw new InvalidDataException(
+				$"pssh DataSize is {initDataSize} bytes, but {remaining} bytes remain in the box.");
+		if (initDataSize > int.MaxValue)
+			throw new InvalidDataException($"pssh DataSize {initDataSize} exceeds the supported array size.");
+
+		InitData = file.ReadBlock((int)initDataSize);
 	}
 
 	protected override void Render(Stream file)
 	{
 		base.Render(file);
 		file.Write(ProtectionSystemId.ToByteArray(bigEndian: true));
-		file.WriteInt32BE(InitData.Length);
+		if (Version == 1)
+		{
+			file.WriteUInt32BE((uint)KIDs.Length);
+			foreach (Guid kid in KIDs)
+				file.Write(kid.ToByteArray(bigEndian: true));
+		}
+		file.WriteUInt32BE((uint)InitData.Length);
 		file.Write(InitData);
-		file.Write(ExtraData);
+	}
+
+	private static void EnsureRemaining(long remaining, long required, string field)
+	{
+		if (required < 0 || remaining < required)
+			throw new InvalidDataException(
+				$"pssh {field} requires {required} bytes, but only {remaining} bytes remain in the box.");
 	}
 }
